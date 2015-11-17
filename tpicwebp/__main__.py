3b0667b016ff744572014b0ea49aea34853f0c71
Tydus <Tydus@Tydus.org>
2015-11-17 11:53:21 +0900

    Add prompts

--- tpicwebp/__main__.py
+++ tpicwebp/__main__.py
@@ -13,15 +13,15 @@ import StringIO
 class MainHandler(tornado.web.RequestHandler):
     @tornado.gen.coroutine
     def get(self):
-        res = yield tornado.httpclient.AsyncHTTPClient().fetch(
-            "%s://%s%s" % (self.request.protocol, self.request.host, self.request.uri)
-        )
+        res = yield tornado.httpclient.AsyncHTTPClient().fetch("%s://%s%s" % (
+            self.request.protocol, self.request.host, self.request.uri,
+        ))
+        print "%s: %s" % (self.request.uri, res.headers["Content-Type"])
 
         if res.headers["Content-Type"] == "image/webp":
             s = StringIO.StringIO()
             PIL.Image.open(res.buffer).save(s, "jpeg")
             self.set_header("Content-Type", "image/jpeg")
-            print "%s is webp" % (self.request.uri)
         else:
             s = res.buffer
             self.set_header("Content-Type", res.headers["Content-Type"])
@@ -33,14 +33,16 @@ def main(argv=None):
         argv = sys.argv[1:]
 
     if len(argv) != 1:
-        print >> sys.stderr, "txwebp <listen port>"
+        print >> sys.stderr, "tpicwebpd <listen port>"
         exit(-1)
 
     tornado.httpserver.HTTPServer(tornado.web.Application([
         ("/.*", MainHandler),
     ]), xheaders = True).listen(int(argv[0]))
 
+    print >> sys.stderr, "tpicwebpd listen on port %s" % argv[0]
     tornado.ioloop.IOLoop.instance().start()
+    print >> sys.stderr, "tpicwebpd shutdown"
 
 if __name__ == '__main__':
     main()
